a4a781be9b10eb7d0e36e00b15e60ce71ad323b1
Gabriel Rodriguez <grodriguez@wuelto.com.mx>
2015-12-02 18:37:17 -0430

    :star: Changing icon launcher

---
+++ app/src/main/java/grodriguez/com/deliveriutest/activities/MainActivity.java
@@ -0,0 +1,16 @@
+package grodriguez.com.deliveriutest.activities;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+import grodriguez.com.deliveriutest.R;
+
+
+public class MainActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
