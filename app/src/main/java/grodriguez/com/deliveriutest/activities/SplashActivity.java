b0e5fab5a4cb56d08f5f48f5f038a4ac6c2a6730
Gabriel Rodriguez <grodriguez@wuelto.com.mx>
2015-12-02 18:44:29 -0430

    :star: Splash Activity to confirm internet connection and present the app

---
+++ app/src/main/java/grodriguez/com/deliveriutest/activities/SplashActivity.java
@@ -0,0 +1,78 @@
+package grodriguez.com.deliveriutest.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.FragmentActivity;
+import android.view.Window;
+import android.view.WindowManager;
+
+import grodriguez.com.deliveriutest.R;
+import grodriguez.com.deliveriutest.dialog.ConfirmationDialog;
+import grodriguez.com.deliveriutest.helpers.ConnectionManager;
+import grodriguez.com.deliveriutest.listeners.OnConfirmationDialogClickListener;
+import grodriguez.com.deliveriutest.utils.Constants;
+
+public class SplashActivity extends FragmentActivity implements OnConfirmationDialogClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+
+        if (ConnectionManager.isConnected(this)) {
+            /**
+             * Time Before Display Main Activity
+             */
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent splashIntent = new Intent(SplashActivity.this, MainActivity.class);
+                    startActivity(splashIntent);
+                    this.finish();
+                }
+
+                private void finish() {
+
+                }
+
+            }, Constants.SPLASH_INTERVAL);
+
+        } else {
+            showConfirmationDialog();
+        }
+
+    }
+
+    /**
+     * Show Dialog to confirm Network Connection
+     */
+
+    private void showConfirmationDialog() {
+
+        String message = getString(R.string.net);
+        String yes = getString(R.string.retry);
+        String no = getString(R.string.exit);
+        String title = getString(R.string.warning_net);
+        new ConfirmationDialog(this, title, yes, no, message, this);
+    }
+
+    @Override
+    public void onConfirm() {
+        if (!ConnectionManager.isConnected(getBaseContext())) {
+            showConfirmationDialog();
+        }
+        if (ConnectionManager.isConnected(getBaseContext())) {
+            Intent i = new Intent(SplashActivity.this, SplashActivity.class);
+            startActivity(i);
+        }
+    }
+
+    @Override
+    public void onRelease() {
+        finish();
+    }
+}
